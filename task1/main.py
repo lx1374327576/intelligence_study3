6fe8e589b3519cc0db36f8a9818d588ea4731b87
lx1374327576 <41529712+lx1374327576@users.noreply.github.com>
2019-05-14 17:24:18 +0800

    train
    train_acc 0.96
    val_acc 0.92

--- task1/main.py
+++ task1/main.py
@@ -10,10 +10,10 @@ from solver import Solver
 
 mnist = MNIST(shuffle=True)
 data = dict()
-data['X_train'] = mnist.X[:100].reshape((100, 1, 28, 28))
-data['y_train'] = mnist.y[:100]
-data['X_val'] = mnist.X[100:200].reshape((100, 1, 28, 28))
-data['y_val'] = mnist.y[100:200]
+data['X_train'] = mnist.X[:500].reshape((500, 1, 28, 28))
+data['y_train'] = mnist.y[:500]
+data['X_val'] = mnist.X[500:600].reshape((100, 1, 28, 28))
+data['y_val'] = mnist.y[500:600]
 model = Cnn()
-solver = Solver(model, data, learning_rate=4e-1, batch_size=50, times=120)
+solver = Solver(model, data, learning_rate=6e-1, batch_size=50, times=120)
 solver.train()
